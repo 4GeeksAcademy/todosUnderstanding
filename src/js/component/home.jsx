0ed2e00f5c75b8f375005b9161f4cf5f96a3ef42
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-09-16 05:24:01 +0000

    useStateUnderstanding

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faTrash } from '@fortawesome/free-solid-svg-icons'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue]= useState("")
+	const [list, setList]=useState([])
+
+	function saveList(e){
+		if(e.key === "Enter"){
+			console.log("Enter was pressed");
+			// setList(list.concat(inputValue))
+			setList(current => ([...current, inputValue]));
+			setInputValue("") 
+		} 
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="text-center" style={{background: "beige", width: "50%", margin: "auto"}}>
+				<h1 className="text-center mt-5">To Do List </h1>
+				<ul>
+					<li> <input type="text" placeholder="What do you need to do?"
+					onChange={(e) => setInputValue(e.target.value)} 
+					value={inputValue}
+					onKeyDown={saveList}
+					style={{background: "beige"}}
+					/></li> 
+					
+					{list.map((item, index) =>(
+						<li> 
+							{item} {" "}
+							<FontAwesomeIcon icon={faTrash} 
+							onClick={() => 
+								setList(
+									list.filter(
+										(l,currentIndex) =>
+											index != currentIndex
+									)
+								)
+							}/>
+						</li>					
+					)
+					)}
+				
+					{/* {list.map ((element) => <li>{element} <FontAwesomeIcon icon={faTrash} /></li> )} */}
+					
+					<li>
+						<div>{list.length} Task</div>
+					</li>
+				</ul>
+				
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
